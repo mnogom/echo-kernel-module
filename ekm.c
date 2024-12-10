be73597a3407e3dd2d42aaff01312b71f4943868
Konstantin Freidlin <freidlinks@cloudx.group>
2024-12-10 18:13:08 +0300

    Add netlink to kernel

--- ekm.c
+++ ekm.c
@@ -1,17 +1,73 @@
+/*
+Sources:
+1. Hello, World: https://www.cyberciti.biz/tips/compiling-linux-kernel-module.html
+2. Netlink example: https://dev.to/zqiu/netlink-communication-between-kernel-and-user-space-2mg1
+3. https://docs.kernel.org/userspace-api/netlink/intro.html
+*/
+
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
 
+#include <net/sock.h>
+#include <linux/netlink.h>
+#include <linux/skbuff.h>
+
+#define EKM_NETLINK 30
+
+struct sock *nl_sk = NULL;
+
+static void recv_msg(struct sk_buff *skb)
+{
+  printk(KERN_INFO "[EKM] --> Entering in :%s\n", __FUNCTION__);
+
+  struct nlmsghdr *nlhead;
+  struct sk_buff *skb_out;
+  int pid, res, msg_size;
+  char *msg = "Hello, from kenel";
+
+  msg_size = strlen(msg);
+  nlhead = (struct nlmsghdr*)skb->data;
+  printk(KERN_INFO "[ELM] --> Receive: %s\n", (char*)nlmsg_data(nlhead));
+  pid = nlhead->nlmsg_pid;
+
+  skb_out = nlmsg_new(msg_size, 0);
+  if(!skb_out)
+  {
+    printk(KERN_ERR "[ELM] --> Failed to allocate new skb\n");
+    return;
+  }
+
+  nlhead = nlmsg_put(skb_out, 0, 0, NLMSG_DONE, msg_size, 0);
+  NETLINK_CB(skb_out).dst_group = 0;
+  strncpy(nlmsg_data(nlhead), msg, msg_size);
+  res = nlmsg_unicast(nl_sk, skb_out, pid);
+
+  if(res < 0)
+    printk(KERN_INFO "[EKM] --> Error while sending response to user\n");
+}
+
 static int __init hello_start(void)
 {
   printk(KERN_INFO "[EKM] Loading module ...\n");
-  printk(KERN_INFO "[EKM] Hello, World!\n");
+
+  struct netlink_kernel_cfg cfg = {
+    .input = recv_msg,
+  };
+  nl_sk = netlink_kernel_create(&init_net, EKM_NETLINK, &cfg);
+  if(!nl_sk) {
+    printk(KERN_ALERT "[EKM] Error while creating socket\n");
+    return -10;
+  }
+
+  printk(KERN_INFO "[EKM] Ekm was successfuly inited!\n");
   return 0;
 }
 
 static void __exit hello_end(void)
 {
   printk(KERN_INFO "[EKM] Goodbye!\n");
+  netlink_kernel_release(nl_sk);
 }
 
 module_init(hello_start);
